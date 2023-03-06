5f1681a38c6ead38b2a624831ec9ee63bf460360
Sefa Eren <sefa.eren000@gmail.com>
2023-03-06 22:36:39 +0300

    env_setup updated gunicorn created

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-if [-d "env"]
+if [ -d "env" ] 
 then
-    echo "Python virtual environment already exists"
+    echo "Python virtual environment exists." 
 else
     python3 -m venv env
 fi
 
-echo $PWD
 source env/bin/activate
 
-pip install -r requirements.txt
 
-if [-d "logs"]
+pip3 install -r requirements.txt
+
+if [ -d "logs" ] 
 then
-    echo "logs directory already exists"
+    echo "Log folder exists." 
 else
     mkdir logs
     touch logs/error.log logs/access.log
 fi
 
 sudo chmod -R 777 logs
-echo "logs directory created"
