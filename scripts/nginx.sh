97b8277de66fe46dca6a7d562cf9cdae689a938a
Sefa Eren <sefa.eren000@gmail.com>
2023-03-07 09:47:54 +0300

    testv1

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-sudo cp -rf scripts/app.conf /etc/nginx/sites-available/app
+sudo cp -rf app.conf /etc/nginx/conf.d
 chmod 710 /var/lib/jenkins/workspace/django-cicd
 
-sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx
